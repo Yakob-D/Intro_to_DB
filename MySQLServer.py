107b4756e0f00a8e16cce0994cae7960377e0b9d
Yakob <126866800+Yakob-D@users.noreply.github.com>
2025-01-30 18:28:40 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -7,10 +7,9 @@ try:
         user = "root",
         passwd = "00Jacob83"
     )
-    if conn.is_connected():
-        cursor = conn.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXIST alx_book_store")
-        print("Database 'alx_book_store' created successfully!")
+    cursor = conn.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXIST alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
 except Error as e:
     print(f"Error: {e}")
 finally:
